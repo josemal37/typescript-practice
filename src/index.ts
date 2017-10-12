176f1ef87d524bd7c2b5d31cf0e0fd07d238b815
josemal37 <josemal_37yahoo.es>
2017-10-12 00:07:46 -0400

    add demostration of CuentaCorriente

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Funciona!");
+import { Cuenta, CuentaCorriente } from "./bank/model/bank";
+
+console.log("Typescript Practice");
+console.log("Banco ABC");
+
+//prueba cuenta corriente
+let cuentaCorriente = new CuentaCorriente(1, 0, "BOB");
+console.log("Cuenta corriente");
+console.log("Numero de cuenta", cuentaCorriente.numero);
+console.log("Saldo", cuentaCorriente.saldo);
+console.log("Moneda", cuentaCorriente.moneda);
